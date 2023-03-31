9a825986d295f3fe66a85ac5f9acf73cfb5ec8fe
AlfredoMarquinaMeseguer <alfmarmes@gmail.com>
2023-03-31 19:14:37 +0200

    Comentación

--- AEDII/Practica1_DyV/solucion.cpp
+++ AEDII/Practica1_DyV/solucion.cpp
@@ -5,22 +5,25 @@
 
 using namespace std;
 
-// tamaño minimo de 6
-int casoPequenno = 12;
+// El tamaño con el que se empiezan a realizar las soluciones directas
+const int casoPequenno = 12;
+const int tamannoSolucion = 6;
+const int mitadSolucion = tamannoSolucion / 2;
+// En este set o bolsa pondremos todas las subcadenas posibles
 std::set<std::string> cadenas;
 
 void solucionDirecta(std::string problema, int inicio, int ultimo)
 {
-    /* Este for está aquí por si se cambia el caso pequeño,
+    /* Este "for" está aquí por si se cambia el caso pequeño,
        Si el caso pequeño es 6. Solo se ejecuta un ciclo.*/
-
-    for (int i = inicio; i < ultimo - 5; i++) // Solución iterativa
+    // Se a cambiado a 12
+    for (int i = inicio; i < ultimo - (tamannoSolucion - 1); i++) // Solución iterativa
     {
-        if (cadenas.find(problema.substr(i, 3)) != cadenas.end() &&
-            cadenas.find(problema.substr(i + 3, 3)) != cadenas.end())
+        if (cadenas.find(problema.substr(i, mitadSolucion)) != cadenas.end() &&
+            cadenas.find(problema.substr(i + mitadSolucion, mitadSolucion)) != cadenas.end())
         {
-            // Se muestra el putno de inicio y la cadena
-            cout << i + 1 << " " << problema.substr(i, 6) << endl;
+            // Se muestra el punto de inicio y la cadena
+            cout << i + 1 << " " << problema.substr(i, tamannoSolucion) << endl;
         }
     }
 }
@@ -32,43 +35,22 @@ void combinar(std::string problema, int junta, int tamanno = casoPequenno)
 
     inicio = junta - 5;
     ultimo = junta + 5;
-    
-    /*inicio = ultimo = junta;
-    if (tamanno / 2 < casoPequenno - 1)
-    {
-        inicio -= tamanno / 2;
-        ultimo += tamanno / 2;
-
-        if (tamanno % 2 == 1)
-        {
-            ultimo += 1;
-        }
-    }
-    else
-    {
-        inicio -= casoPequenno - 1;
-        ultimo += casoPequenno - 1;
-    }*/
-    // inicio = junta - (tamanno - 1);
-    // ultimo = junta + (tamanno - 1);
-
-    //  Procuramos que los punteros no apunten fuera del array
-
     solucionDirecta(problema, inicio, ultimo);
 }
 
 void divide(std::string problema, int inicio, int ultimo)
 {
+    // Autoexplicativo
     int tamannoProblema = ultimo - inicio;
-    // cout << "Tamaño problema " << tamannoProblema << endl;
     if (tamannoProblema <= casoPequenno)
     {
-        // Se asume que es de tamaño del caso pequenno
         solucionDirecta(problema, inicio, ultimo);
     }
     else if (tamannoProblema > casoPequenno)
     {
+        // La zona donde se separan los dos subproblemas
         int junta = (inicio + ultimo) / 2;
+        // Misma estrucutra que las torres de Hanoi
         divide(problema, inicio, junta);
         combinar(problema, junta, tamannoProblema);
         divide(problema, junta, ultimo);
@@ -82,22 +64,24 @@ void divideYVenceras(std::string problema)
 
 void iterativa(string problema)
 {
-    int n = problema.length();
-
-    for (int i = 0; i < n - 5; i++)
+    for (int i = 0; i < problema.length() - (tamannoSolucion-1); i++)
     {
-        if (cadenas.find(problema.substr(i, 3)) != cadenas.end() &&
-            cadenas.find(problema.substr(i + 3, 3)) != cadenas.end())
+        if (cadenas.find(problema.substr(i, mitadSolucion)) != cadenas.end() &&
+            cadenas.find(problema.substr(i + mitadSolucion, mitadSolucion)) != cadenas.end())
         {
-            cout << i + 1 << " " << problema.substr(i, 6) << endl;
+            cout << i + 1 << " " << problema.substr(i, tamannoSolucion) << endl;
         }
     }
 }
 
 int main(int argc, char *argv[])
 {
-    string componentes;
-    string problema;
+    // Aquí vamos a leer las subcadenas de patrones a identificar
+    string componentes, 
+    // La cadena continua larga del problema
+        problema;
+
+    // Leemos la cadena de stdin
     for (int i = 0; i < 3; i++)
     {
         getline(std::cin, componentes);
@@ -105,14 +89,13 @@ int main(int argc, char *argv[])
     }
     getline(std::cin, problema);
 
+    // Lee del arg el tipo de solución
     if (argc > 1 && std::strcmp(argv[1], "it") == 0)
     {
-        cout << "it" << endl;
         iterativa(problema);
     }
     else if (argc > 1 && std::strcmp(argv[1], "dyv") == 0)
     {
-        cout << "dyv" << endl;
         divideYVenceras(problema);
     }
     return 0;
