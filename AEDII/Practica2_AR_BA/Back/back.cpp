4cf45b9ca09cbd2f30f34450632609ef7f6521f9
AlfredoMarquinaMeseguer <alfmarmes@gmail.com>
2023-05-09 20:00:29 +0200

    más lento que el caballo del malo

---
+++ AEDII/Practica2_AR_BA/Back/back.cpp
@@ -0,0 +1,21 @@
+#include <string>
+struct Solucion{
+    int * equipoA;
+    int * equipoB;
+};
+
+int diferencia(struct Solucion equipos){
+    // Codigo en phyton
+    // a = reduce(lambda x, y: x + y, equipos.equipoA)
+    // b = reduce(lambda x, y: x + y, equipos.equipoB)
+    // return abs(a - b)
+
+
+    return 0;
+}
+
+struct Solucion backtracking(int * candidatos, int indiceCandidatos, struct Solucion equipos, int dimensiones){
+    if (indiceCandidatos == dimensiones){
+        
+    }
+}
--- AEDII/Practica2_AR_BA/Back/bt.cpp
+++ AEDII/Practica2_AR_BA/Back/bt.cpp
@@ -72,29 +72,58 @@ Solucion* backtracking(vector<int>& candidatos, const Solucion& equipos) {
 }
 
 int main() {
-
+    int problemas, numCandidatos, candidatoActual, pesoA, pesoB;
+    char * aux = new char[1];
     //vector<int> cant = {450, 106, 87, 66, 75, 372, 215, 6, 304, 291, 25, 88, 133, 221, 85, 108, 105, 381, 323, 352, 130, 100};
-    //vector<int> candidatos = {100, 90, 200};
+    vector<int> candidatos;
     vector<int> candidatos2 = {450, 106, 87, 66, 75, 372, 215, 6, 304, 291, 25, 88, 133, 221, 85, 108, 105, 381, 323, 352, 130, 100};
     //Solucion a{{450, 106, 87, 66, 75, 372, 215, 25, 133, 352, 130},
               // {6, 304, 291, 88, 221, 85, 108, 105, 381, 323, 100}};
-    //Solucion a{{100, 90},{200}};
+    //Solucion a{{100, 90},{200}};    
+    // cout << "Cantidad de candidatos: " << candidatos2.size() << endl;
 
-    cout << "Cantidad de candidatos: " << candidatos2.size() << endl;
-    Solucion equipos{{}, {}};
-    auto solucion = backtracking(candidatos2, equipos);
-    if (solucion == nullptr) {
-        cout << "No se encontró solución" << endl;
-    } else {
-        cout << "Equipo A: ";
-        for (const int &elem : solucion->equipoA) {
-            cout << elem << " ";
+    // Entrada
+    cin >> problemas;    
+    // cin.getline();
+    // getline(aux, cin);
+    for(int i =0; i<problemas; ++i) {
+        cin >> numCandidatos;
+        candidatos = {};
+        for(int j=0; j< numCandidatos; ++j) {            
+            cin >> candidatoActual;
+            candidatos.push_back(candidatoActual);
         }
-        cout << endl << "Equipo B: ";
-        for (const int &elem : solucion->equipoB) {
-            cout << elem << " ";
-        }
-        cout << endl << "Diferencia: " << diferencia(*solucion) << endl;
-    }
+        // getline(aux, cin);
+        // Fin Entrada
+
+        Solucion equipos{{}, {}};       
+        auto solucion = backtracking(candidatos, equipos);
+        if (solucion == nullptr) {
+            // No deberÃ­a ocurrir nunca
+            cout << "No se encontrï¿½ soluciï¿½n" << endl;
+        } else {
+            // cout << "Equipo A: ";
+            pesoA = 0;
+            for (const int &elem : solucion->equipoA) {
+                // cout << elem << " ";
+                pesoA += elem;
+            }
+            // cout << endl << "Equipo B: ";
+            pesoB = 0;
+            for (const int &elem : solucion->equipoB) {
+                // cout << elem << " ";
+                pesoB += elem;
+            }
+            // cout << endl << "Diferencia: " << diferencia(*solucion) << endl;
+
+            if (pesoA < pesoB) 
+                cout << pesoA << " " << pesoB << endl;
+            else
+                cout << pesoB << " " << pesoA << endl;
+            
+        }            
+    }   
+
     return 0;
 }
+
