3d5e4cb147269c14af75e8d9140b815c918ea000
AlfredoMarquinaMeseguer <alfmarmes@gmail.com>
2023-06-15 19:43:07 +0200

    Terminar backtracking

--- AEDII/Practica2_AR_BA/Back/back.cpp
+++ AEDII/Practica2_AR_BA/Back/back.cpp
@@ -1,115 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-void backtracking(int *candidatos, int n, bool *elementosActuales, int elementosSelecionados,
-                  bool *equipos, int *diferenciaMin, int suma, int sumaActual, int posicionActual)
+bool *backtracking_v2(int *candidatos, int n)
 {
-    
-    if ((posicionActual == n) || ((n / 2 - elementosSelecionados) > (n - posicionActual)))
-        return;
-
-    
-    backtracking(candidatos, n, elementosActuales, elementosSelecionados,
-                 equipos, diferenciaMin, suma, sumaActual, posicionActual + 1);
-
-    
-    elementosSelecionados++;
-    sumaActual += candidatos[posicionActual];
-    elementosActuales[posicionActual] = true;
-
-    // Comprobamos que sea un nodo hoja
-    if (elementosSelecionados == n / 2)
-    {
-        // Si la solución obtenida es menor que la mínima, sobreescribir
-        if (abs(suma / 2 - sumaActual) < *diferenciaMin)
-        {
-            *diferenciaMin = abs(suma / 2 - sumaActual);
-            for (int i = 0; i < n; i++)
-                equipos[i] = elementosActuales[i];
-        }
-    }
-    else
-    {
-        backtracking(candidatos, n, elementosActuales, elementosSelecionados, equipos,
-                     diferenciaMin, suma, sumaActual, posicionActual + 1);
-    }
-
-    elementosActuales[posicionActual] = false;
-}
-
-
-void resolver(int *candidatos, int n)
-{
-    bool *elementoActual = new bool[n];
-
-
-    bool *solucion = new bool[n];
-
-    int difrenciaMin = INT_MAX;
-
-    int suma = 0;
-    for (int i = 0; i < n; i++)
-    {
-        suma += candidatos[i];
-        elementoActual[i] = false;
-        solucion[i] = false;
-    }
-
-
-    backtracking(candidatos, n, elementoActual, 0, solucion, &difrenciaMin, suma, 0, 0);
-
-    int total1 = 0;
-    int  total2 = 0;
-    for (int i = 0; i < n; i++)
-    {
-        if (solucion[i] == true)
-        {
-            total1 += candidatos[i];
-        }
-        else
-        {
-            total2 += candidatos[i];
-        }
-    }
-
-    if (total1 < total2)
-    {
-        cout << total1 << " " << total2 << endl;
-    }
-    else
-    {   
-        cout << total2 << " " << total1 << endl;
-    }
-}
-
-
-bool * backtracking_v2(int* candidatos, int n )
-{    
-    
-    int elementosSelecionados=0, 
-        suma = 0, 
-        sumaActual = 0, 
-        posicionActual = 0;
+    int elementosSelecionados = 0,
+        sumaActual = 0;
     bool *elementosActuales = new bool[n];
     bool *solucion = new bool[n];
-    int diferenciaMin = INT_MAX;
-    for (int i = 0; i < n; i++) {
+    float diferenciaMin = INT_MAX;
+    float suma = 0;
+    for (int i = 0; i < n; i++)
+    {
+        // cout << candidatos[i] << " " << suma << endl;
         suma += candidatos[i];
         elementosActuales[i] = false;
         solucion[i] = false;
     }
 
-    int iteraciones = pow(2,n);
-    int i = iteraciones/2;
-    // Coinducición de todas las posibilidades y segunda condición de solución
-    while ( i < iteraciones and diferenciaMin != 0 ) {
+    int iteraciones = pow(2, n - 1);
+    int i = 0;
+    // Condición de todas las posibilidades y segunda condición de solución
+    // and diferenciaMin != 0
+    float minimo = n % 2 / 2.0;
+    do
+    {
         // Reiniciar variables en cada iteración
         elementosSelecionados = 0;
         sumaActual = 0;
         // Función genera este bucle
+        //&& sumaActual <= (suma / 2)
         for (int j = 0; j < n; j++)
         {
+            // Por cada bit de i representado por j se asigna su valor a un array de booleanos
             elementosActuales[j] = (i & (1 << j)) != 0;
             if (elementosActuales[j])
             {
@@ -117,27 +39,28 @@ bool * backtracking_v2(int* candidatos, int n )
                 sumaActual += candidatos[j];
             }
         }
-
         // Comprobar criterio
-        if (elementosSelecionados == n / 2 && abs(suma / 2 - sumaActual) < diferenciaMin) // Criterio
+        if ((elementosSelecionados == n / 2 || elementosSelecionados == n / 2 + n % 2) && abs((suma / 2) - sumaActual) < diferenciaMin) // Criterio
         {
-            diferenciaMin = abs(suma / 2 - sumaActual);
+
+            diferenciaMin = abs((suma / 2) - sumaActual);
+            // cout <<  diferenciaMin << " " << suma << " " <<  sumaActual << endl;
             for (int j = 0; j < n; j++)
                 solucion[j] = elementosActuales[j];
         }
         i++;
-    }
-    
+    }while (i < iteraciones and diferenciaMin != minimo);
+
     delete elementosActuales;
     return solucion;
 }
 
 void resolver_v2(int *candidatos, int n)
 {
-    bool * solucion = backtracking_v2(candidatos, n);
+    bool *solucion = backtracking_v2(candidatos, n);
 
     int total1 = 0;
-    int  total2 = 0;
+    int total2 = 0;
     for (int i = 0; i < n; i++)
     {
         if (solucion[i] == true)
@@ -152,11 +75,11 @@ void resolver_v2(int *candidatos, int n)
 
     if (total1 < total2)
     {
-        cout << total1 << " " << total2 << endl;
+        std::cout << total1 << " " << total2 << std::endl;
     }
     else
-    {   
-        cout << total2 << " " << total1 << endl;
+    {
+        std::cout << total2 << " " << total1 << std::endl;
     }
     delete solucion;
 }
@@ -168,17 +91,16 @@ int main()
     cin >> problemas;
     while (problemas--)
     {
-        cin >> numCandidatos;        
+        cin >> numCandidatos;
 
         int *candidatos = new int[numCandidatos];
         for (int j = 0; j < numCandidatos; ++j)
         {
             cin >> candidatoActual;
-            candidatos[j] =  candidatoActual;
+            candidatos[j] = candidatoActual;
         }
-        // resolver(candidatos, numCandidatos);         
-        resolver_v2(candidatos, numCandidatos); 
-        // cout << "*******************************" << endl;
+        // resolver(candidatos, numCandidatos);
+        resolver_v2(candidatos, numCandidatos);
         delete candidatos;
     }
     return 0;
